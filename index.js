f5fdfd73042b51456d7b5f93481aabf66e94b0ae
Vanshikaa <vanshika31032000@gmail.com>
2020-06-14 10:19:07 +0530

    Node Mongoose Part 2

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const url ='mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log('Connected correctly to server');
-    var newDish = Dishes({
+
+  Dishes.create({
         name: 'Uthappizza',
         description: 'test'
-    });
-    newDish.save().then((dish) => {
+    })
+    .then((dish) => {
         console.log(dish);
-        Dishes.find({}).exec();
+      return  Dishes.find({}).exec();
     })
     .then((dishes) => {
         console.log(dishes);
