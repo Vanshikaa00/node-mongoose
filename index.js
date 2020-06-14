444788266b90ee20d8a0f84446eb5665a77892c0
Vanshikaa <vanshika31032000@gmail.com>
2020-06-14 10:47:43 +0530

    Node Mongoose Part 3

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
-const url ='mongodb://localhost:27017/conFusion';
+const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log('Connected correctly to server');
 
-  Dishes.create({
-        name: 'Uthappizza',
+    Dishes.create({
+        name: 'Uthappizzaas',
         description: 'test'
     })
     .then((dish) => {
         console.log(dish);
-<<<<<<< HEAD
-      return  Dishes.find({}).exec();
-=======
-       return Dishes.find({}).exec();
->>>>>>> 0e1ef902a055102a70222ba5472df2b74845c8af
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
-        return Dishes.remove();
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.remove({});
     })
     .then(() => {
         return mongoose.connection.close();
@@ -27,4 +41,5 @@ connect.then((db) => {
     .catch((err) => {
         console.log(err);
     });
+
 });
